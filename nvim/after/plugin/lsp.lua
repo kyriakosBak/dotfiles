603a9fdd45616d7c02e72e5c093e428e9a7f1986
Kyriakos <kyriakos.bakirlis@gmail.com>
2024-02-01 15:29:26 +0100

    adding nvim configuration

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,23 @@
+local lsp = require('lsp-zero')
+
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>ref", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp.default_setup,
+  },
+})
+
+lsp.preset("recommended")
+
+lsp.setup()
